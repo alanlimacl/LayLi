e33a021a9a60c7ed31e5a9772ac6cd1ef1024d08
Alan Lima <alancoite123@hotmail.com>
2026-09-07 16:20:31 -0300

    Ajuste pro mano Hugo

--- controller/rota_requisicao.py
+++ controller/rota_requisicao.py
@@ -1,2 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 
+roteador_requisicao = APIRouter(prefix="/requisitar", tags=["Requisitar"])
+
+@roteador_requisicao.post("/")
+async def root():
+    return {"messages": "Rota Root da Requisição"}
+
+
+@roteador_requisicao.post("/")
+async def rota_requisicao():
+    
